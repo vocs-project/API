856fb8f282f27af8841666575f95e4e39da5e016
unknown <etienne.leba@gmail.com>
2018-01-20 21:13:43 +0100

    del unique to list wordtrad relation

--- src/VOCS/PlatformBundle/Entity/Lists.php
+++ src/VOCS/PlatformBundle/Entity/Lists.php
@@ -48,7 +48,7 @@ class Lists
      * @ORM\ManyToMany(targetEntity="WordTrad", cascade={"remove"})
      * @ORM\JoinTable(name="list_wordTrad",
      *      joinColumns={@ORM\JoinColumn(name="list_id", referencedColumnName="id")},
-     *      inverseJoinColumns={@ORM\JoinColumn(name="wordTrad_id", referencedColumnName="id", unique=true, onDelete="CASCADE")}
+     *      inverseJoinColumns={@ORM\JoinColumn(name="wordTrad_id", referencedColumnName="id", onDelete="CASCADE")}
      *      )
      */
     private $wordTrads;
